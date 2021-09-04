088e4e98e8848cc7e35df5ce1d68e31921fa92f9
zhangxiangchen <87363706+zhangxiangchen@users.noreply.github.com>
2021-09-04 14:05:52 +0800

    Add files via upload

---
+++ zxc_os/zxc_os/neofetch.h
@@ -0,0 +1,22 @@
+using namespace std;
+void neofetch(){
+cout<<endl;
+cout<<"                    'c.          zxc@zxcdeMacBook-Pro.local"<<endl;
+cout<<"                 ,xNMM.          --------------------------"<<endl;
+cout<<"               .OMMMMo           OS: macOS 11.2 20D64 x86_64"<<endl;
+cout<<"               OMMM0,            Host: Hackintosh (SMBIOS: MacBookPro16,3)"<<endl;
+cout<<"     .;loddo:' loolloddol;.      Kernel: 20.3.0"<<endl;
+cout<<"   cKMMMMMMMMMMNWMMMMMMMMMM0:    Uptime: 1 hour, 9 mins"<<endl;
+cout<<" .KMMMMMMMMMMMMMMMMMMMMMMMWd.    Packages: 37 (brew)"<<endl;
+cout<<" XMMMMMMMMMMMMMMMMMMMMMMMX.      Shell: zsh 5.8"<<endl;
+cout<<";MMMMMMMMMMMMMMMMMMMMMMMM:       Resolution: 1600x900"<<endl;
+cout<<":MMMMMMMMMMMMMMMMMMMMMMMM:       DE: Aqua"<<endl;
+cout<<".MMMMMMMMMMMMMMMMMMMMMMMMX.      WM: Quartz Compositor"<<endl;
+cout<<" kMMMMMMMMMMMMMMMMMMMMMMMMWd.    WM Theme: Blue (Dark)"<<endl;
+cout<<" .XMMMMMMMMMMMMMMMMMMMMMMMMMMk   Terminal: iTerm2"<<endl;
+cout<<"  .XMMMMMMMMMMMMMMMMMMMMMMMMK.   Terminal Font: JetBrainsMono-Regular 15"<<endl;
+cout<<"    kMMMMMMMMMMMMMMMMMMMMMMd     CPU: Intel i5-10210U (8) @ 1.60GHz"<<endl;
+cout<<"     ;KMMMMMMMWXXWMMMMMMMk.      GPU: Intel Iris Plus Graphics 655"<<endl;
+cout<<"       .cooc,.    .,coo:.        Memory: 6376MiB / 16384MiB"<<endl;
+cout<<endl;
+}
